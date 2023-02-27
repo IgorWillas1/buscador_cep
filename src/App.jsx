22f8509fa1a26ee3d38abbb42479b37424733c70
Igor Willas <igorwillas1@gmail.com>
2023-02-27 14:38:16 -0300

    finalizando o back-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
 import './style.app.css';
+import api from './services/api.js';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSearch = async () => {
+    if (input === '' || input === undefined) {
+      alert('por favor, digite um CEP!')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+
+    }catch (err) {
+      alert('Erro ao buscar CEp, por favor digite um CEP válido.')
+    }
+  }
+  
+
   return (
     <div className="container">      
       <h1 className="title">Buscador de CEP</h1>
@@ -10,24 +37,29 @@ function App() {
 
         <input 
         type="text"
-        id="search"
         placeholder="Digite um CEP válido..."
+        value={input}
+        onChange={handleInput}
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch"
+        onClick={handleSearch}
+        >
           <FiSearch size={25} color='#000' />
         </button>
 
       </div>
 
-      <main className='main'>
-        <h2>CEP: 55198409</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+        <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua: Maria sei lá</span>
-        <span>Complemento: perto da rua</span>
-        <span>Bairro: São João</span>
-        <span>Cidade: Santa Cruz - PE</span>
+        <span>Rua: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento || 'Não existe complemento'}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade + '-' + cep.uf }</span>
       </main>
+      )}
 
     </div>
   );
