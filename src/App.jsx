3a61b4b181533b37761d156e516fdd42445dfe82
Igor Willas <igorwillas1@gmail.com>
2023-02-27 13:35:11 -0300

    finalizando o front

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { FiSearch } from 'react-icons/fi'
+import './style.app.css';
+
 function App() {
   return (
-    <>
-      <h1>Olá</h1>
-    </>
+    <div className="container">      
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+
+        <input 
+        type="text"
+        id="search"
+        placeholder="Digite um CEP válido..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#000' />
+        </button>
+
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 55198409</h2>
+
+        <span>Rua: Maria sei lá</span>
+        <span>Complemento: perto da rua</span>
+        <span>Bairro: São João</span>
+        <span>Cidade: Santa Cruz - PE</span>
+      </main>
+
+    </div>
   );
 }
 
